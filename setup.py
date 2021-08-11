d941f9bd457caaf13ed7cb35a4ef3f62347e8426
nathann3 <59952641+nathann3@users.noreply.github.com>
2021-08-11 10:25:25 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict the best price to list your Airbnb based on thousands of listings in Seattle',
+    author='Nathan Nguyen',
+    license='',
+)
